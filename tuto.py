720c785f0bc9eea2cbe8f18c10a2e28ed1510e57
fabffo <ffougery@hotmail.com>
2021-09-04 23:55:12 +0200

    Première version sur l'ensemble des pages

--- tuto.py
+++ tuto.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-url = 'http://books.toscrape.com/'
+links = []
 
-response = requests.get(url)
+#Boucle sur la totalité des pages
+for i in range(50):
+    urlOrigin = 'http://books.toscrape.com/'
+    url = 'http://books.toscrape.com/catalogue/page-' + str(i) + '.html'
+    response = requests.get(url)
 
-if response.ok:
-    links = []
-    soup = BeautifulSoup(response.text, features="html.parser")
-    articles = soup.find_all('article')
-    for article in articles:
-        #print(article)
-        a = article.find('a')
-        link = a['href']
-        #print(link)
-        links.append(url + link)
-    print(links)
+   #On récupère une page et on l'analyse
+    if response.ok:
+        soup = BeautifulSoup(response.text, features="html.parser")
+        articles = soup.find_all('article')
+
+        for article in articles:
+            # print(article)
+            a = article.find('a')
+            link = a['href']
+            # print(link)
+            links.append(urlOrigin + link)
+
+        #On temporise 1 seconde pour ne pas se faire sortir
+        time.sleep(1)
+
+print(links)
